119803ee2d6f1b6f93cf494ff6cbe6875979e2e8
James Reeves <jreeves@weavejester.com>
2014-02-15 17:39:47 +0000

    Added license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject crypto-random "1.1.0"
   :description "Library for generating secure random bytes and strings"
   :url "https://github.com/weavejester/crypto-random"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [commons-codec "1.6"]])
